5ba4231019f732482fe90e3e813553d0bc19a32a
Daniel Vasilev <61119347+aANKFcrkfR@users.noreply.github.com>
2020-04-07 11:57:53 +0300

    Homework

---
+++ Homework-09-04-2020/Phonebook/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> contacts = new Dictionary<string, string>();
+            while(true)
+            {
+                var input = Console.ReadLine().Split(' ');
+                string command = input[0].ToLower();
+                switch (command)
+                {
+                    case "a":      
+                        {
+                            contacts.Add(input[1], input[2]);
+                            break;
+                        }
+                    case "s": 
+                        {
+                            if (contacts.ContainsKey(input[1]))
+                            {
+                                Console.WriteLine($"{input[1]} -> {contacts[input[1]]}");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"This contact does not exist.");
+                            }
+                            break;
+                        }
+                    case "end": 
+                        {
+                            return;
+                        }
+                }
+            }
+        }
+    }
+}
---
+++ Homework-09-04-2020/minecraft/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minecraft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> resources = new Dictionary<string, int>();
+            resources.Add("Gold", 0);
+            resources.Add("Silver", 0);
+            resources.Add("Copper", 0);
+            while (true)
+            {
+                Console.Write("Enter type of metal: ");
+                string comm = Console.ReadLine().ToLower();
+                if (comm == "stop" || comm == "copper" || comm == "silver" || comm == "gold")
+                {
+                    if (comm != "stop") 
+                    {
+                        int amount = 0;
+                        Console.Write($"Enter amount {comm}: ");
+                        try
+                        {
+                            amount = int.Parse(Console.ReadLine());
+                        }
+                        catch (FormatException e) { Console.WriteLine("You must type a number."); }
+                        switch (comm)
+                        {                            
+                            case "copper":
+                                {
+                                    resources["Copper"] += amount;
+                                    Console.WriteLine($"{amount} copper added.");
+                                    break;
+                                }
+                            case "silver":
+                                {
+                                    resources["Silver"] += amount;
+                                    Console.WriteLine($"{amount} silver added.");
+                                    break;
+                                }
+                            case "gold":
+                                {
+                                    resources["Gold"] += amount;
+                                    Console.WriteLine($"{amount} gold added.");
+                                    break;
+                                }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Gold -> {resources["Gold"]}");     
+                        Console.WriteLine($"Silver -> {resources["Silver"]}"); 
+                        Console.WriteLine($"Copper -> {resources["Copper"]}"); 
+                        break;
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
