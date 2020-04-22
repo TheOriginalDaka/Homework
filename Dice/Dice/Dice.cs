4f4e3423be4c0b0c3eafd77beb651ebb79e1e337
Daniel Vasilev <61119347+aANKFcrkfR@users.noreply.github.com>
2020-04-22 18:03:43 +0300

    Uploaded

---
+++ Dice/Dice/Dice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice
+{
+    class Dice
+    {
+        Random random = new Random();
+        public int RollDice(int size)
+        {
+            int randomNumber = random.Next(1, size + 1);
+            return randomNumber;
+        }
+    }
+}
---
+++ Dice/Dice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dice dice = new Dice();
+            int result = 0;
+
+            Console.Write("Enter size: ");
+            int size = int.Parse(Console.ReadLine());
+
+            result = dice.RollDice(size);
+            Console.WriteLine("The number is: " + result);
+
+            Console.ReadLine();
+        }
+    }
+}
